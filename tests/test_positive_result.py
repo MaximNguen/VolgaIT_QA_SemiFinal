8fa9d2dd971e730ce2f915450a8076df1820d8ac
MaximNguyen <foditoon1@gmail.com>
2025-11-19 14:55:29 +0300

    Done with positive tests

--- tests/test_positive_result.py
+++ tests/test_positive_result.py
@@ -52,21 +52,21 @@ class TestFormsPage:
         current_url = browser.current_url
         assert expected_url == current_url, \
             f"Ожидалось, что после подтверждения браузер перейдет нa - {expected_url}, но браузер перешел на {current_url}"
-        assert first_name_field_status == True, f"Ожидалось, что поле будет пустый после подтверждения, но в поле осталось текст {self.form_page.first_name_find().text}"
-        assert last_name_field_status == True, f"Ожидалось, что поле будет пустый после подтверждения, но в поле осталось текст {self.form_page.last_name_find().text}"
+        assert first_name_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.first_name_find().text}"
+        assert last_name_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.last_name_find().text}"
         assert checkbox1_field_status == True, ET.COMMENT_ABOUT_BOX
         assert checkbox2_field_status == True, ET.COMMENT_ABOUT_BOX
         assert checkbox3_field_status == True, ET.COMMENT_ABOUT_BOX
-        assert phone_number_field_status == True, f"Ожидалось, что поле будет пустый после подтверждения, но в поле осталось текст {self.form_page.phone_number_find().text}"
-        assert username_field_status == True, f"Ожидалось, что поле будет пустый после подтверждения, но в поле осталось текст {self.form_page.username_find().text}"
-        assert email_field_status == True, f"Ожидалось, что поле будет пустый после подтверждения, но в поле осталось текст {self.form_page.email_find().text}"
-        assert password_field_status == True, f"Ожидалось, что поле будет пустый после подтверждения, но в поле осталось текст {self.form_page.password_find().text}"
-        assert confirm_password_field_status == True, f"Ожидалось, что поле будет пустый после подтверждения, но в поле осталось текст {self.form_page.password_confirm_find().text}"
+        assert phone_number_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.phone_number_find().text}"
+        assert username_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.username_find().text}"
+        assert email_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.email_find().text}"
+        assert password_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.password_find().text}"
+        assert confirm_password_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.password_confirm_find().text}"
 
     @allure.feature("Положительный тест-кейс №2")
     @allure.story("Ввод всех полей")
     @pytest.mark.parametrize("first_name, last_name, radio, phone_number, username, mail, photo, about, password", TD.DataForAllField)
-    def test_all_fields(self, first_name, last_name, radio, phone_number, username, mail, photo, about, password):
+    def test_all_fields(self, browser, first_name, last_name, radio, phone_number, username, mail, photo, about, password):
         self.form_page.first_name_input(first_name)
         self.form_page.last_name_input(last_name)
         match radio:
@@ -90,3 +90,37 @@ class TestFormsPage:
         self.form_page.password_input(password)
         self.form_page.password_confirm_input(password)
         self.form_page.submit_click()
+
+        first_name_field_status = ET.expected_text(self.form_page.first_name_find(), "")
+        last_name_field_status = ET.expected_text(self.form_page.last_name_find(), "")
+        radiobox1_field_status = ET.expected_text(self.form_page.radiobox1_find(), "")
+        radiobox2_field_status = ET.expected_text(self.form_page.radiobox2_find(), "")
+        radiobox3_field_status = ET.expected_text(self.form_page.radiobox3_find(), "")
+        checkbox1_field_status = ET.boxState(self.form_page.checkbox1_find())
+        checkbox2_field_status = ET.boxState(self.form_page.checkbox2_find())
+        checkbox3_field_status = ET.boxState(self.form_page.checkbox3_find())
+        phone_number_field_status = ET.expected_text(self.form_page.phone_number_find(), "")
+        username_field_status = ET.expected_text(self.form_page.username_find(), "")
+        email_field_status = ET.expected_text(self.form_page.email_find(), "")
+        about_field_status = ET.expected_text(self.form_page.about_find(), "")
+        password_field_status = ET.expected_text(self.form_page.password_find(), "")
+        confirm_password_field_status = ET.expected_text(self.form_page.password_confirm_find(), "")
+
+        expected_url = TD.EXPECTED_URL
+        current_url = browser.current_url
+        assert expected_url == current_url, \
+            f"Ожидалось, что после подтверждения браузер перейдет нa - {expected_url}, но браузер перешел на {current_url}"
+        assert first_name_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.first_name_find().text}"
+        assert last_name_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.last_name_find().text}"
+        assert radiobox1_field_status == True, ET.COMMENT_ABOUT_BOX
+        assert radiobox2_field_status == True, ET.COMMENT_ABOUT_BOX
+        assert radiobox3_field_status == True, ET.COMMENT_ABOUT_BOX
+        assert checkbox1_field_status == True, ET.COMMENT_ABOUT_BOX
+        assert checkbox2_field_status == True, ET.COMMENT_ABOUT_BOX
+        assert checkbox3_field_status == True, ET.COMMENT_ABOUT_BOX
+        assert phone_number_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.phone_number_find().text}"
+        assert username_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.username_find().text}"
+        assert email_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.email_find().text}"
+        assert about_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.about_find().text}"
+        assert password_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.password_find().text}"
+        assert confirm_password_field_status == True, f"{ET.EXPECTED_EMPTY_TEXT} {self.form_page.password_confirm_find().text}"
